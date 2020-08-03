3b6c26c44a7ac239738b98bb0bf72f3dbd1cff84
prodes13 <ciobotaru.florin@gmail.com>
2020-08-03 17:59:21 +0300

    music app example started

---
+++ music-app/app.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+	const sounds = document.querySelectorAll('.sounds');
+	// you can select with this all divs!!!
+	const pads = document.querySelectorAll('.pads div');
+	
+	pads.forEach(pad => {
+		pad.addEventListener('click', function() {
+			
+		});
+	});
+	
+	console.log(sounds);
+});
