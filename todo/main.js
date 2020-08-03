4292de7eed60f58713e7c65e2433a6cca3954bf5
prodes13 <ciobotaru.florin@gmail.com>
2020-08-03 12:35:45 +0300

    layout done

---
+++ todo/main.js
@@ -0,0 +1,54 @@
+// selectors
+
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+// event listeners
+todoButton.addEventListener('click', addTodo);
+todoList.addEventListener('click', deleteCheck);
+// functions
+
+function addTodo(event) {
+    // prevent form from submiting
+    event.preventDefault();
+    
+    // create list of todos
+    const todoDiv = document.createElement('div');
+    todoDiv.classList.add("todo");
+
+    const newTodo = document.createElement('li');
+    newTodo.innerText = todoInput.value;
+    newTodo.classList.add('todo-item');
+
+    todoDiv.appendChild(newTodo);
+
+    // check button
+    const completedButton = document.createElement('button');
+    completedButton.innerHTML = '<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-btn');
+    todoDiv.appendChild(completedButton);
+    // check button
+    const trashButton = document.createElement('button');
+    trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+    trashButton.classList.add('trash-btn');
+    todoDiv.appendChild(trashButton);
+    // append to list
+    todoList.appendChild(todoDiv);
+
+    // clear input value
+    todoInput.value = '';
+}
+
+function deleteCheck(e) {
+    const item = e.target;
+    // delete todo
+    if(item.classList[0] === 'trash-btn') {
+        item.parentElement.remove();
+    }
+
+    // complete todo
+    if(item.classList[0] === 'complete-btn') {
+        item.parentElement.classList.toggle("completed");
+    }
+}
